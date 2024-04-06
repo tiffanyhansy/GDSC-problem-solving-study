03f65be8d732fa894ec13f08e1ab7208abaf396e
Seungyeon Han <tiffanyhansy@gmail.com>
2024-04-07 02:08:52 +0900

    📝 Chore: Search insert position 수정

--- Array/35.py
+++ Array/35.py
@@ -1,3 +1,20 @@
 # Search insert position 
-# target 이 있으면 index 반환, targer 없으면 있어야 할 index 반환 
+# target 이 있으면 index 반환, target 없으면 있어야 할 index 반환 
 
+class Solution(object):
+    def searchInsert(self, nums, target):
+        start = 0
+        end = len(nums) - 1 
+        
+        while start <= end:
+          m = ( start + ( end - start )) // 2
+          
+          if nums[m] == target: #binary search 성공
+            return m
+          
+          elif nums[m] < target:  #binary search에서 target 보다 왼쪽
+            start = m + 1
+            
+          else: end = m - 1 #binary search에서 target 보다 오른쪽
+          
+        return start 
