0178396930df9dc1546bac4a8546637ab9a6ea05
Seungyeon Han <tiffanyhansy@gmail.com>
2024-05-12 21:27:48 +0900

    ✨ Feat:  2011. Final Value of Variable After Performing Operations 풀이

---
+++ Array/2011. Final Value of Variable After Performing Operations.py
@@ -0,0 +1,14 @@
+"""
+Link: https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
+"""
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+      X=0
+      for op in operations:
+        if op == "--X" or op == "X--":
+          X -= 1
+        
+        else: X += 1
+        
+      return X
